65bc352fb5a9f8f6d39e87d8e0da1d69cb19373d
sitheswaran <sitheswaransara@gmail.com>
2023-01-31 11:02:08 +0530

    updated

--- js/main.js
+++ js/main.js
@@ -7,15 +7,68 @@ usrInp.style.width="50%";
 usrInp.style.padding="30px";
 
 var usrAge1=document.createElement("input");
+
 var usrAge2=document.createElement("input");
+
 var usrAge3=document.createElement("input");
 var usrBtn=document.createElement("button");
+
 var usrResult=document.createElement("textarea");
 usrBtn.innerHTML="Result";
 
-usrResult.setAttribute("id","view");
+usrResult.setAttribute("id","content");
 usrResult.setAttribute("value","");
+
 usrResult.setAttribute("placeholder","OUTPUT VALUE");
+
+
+usrInp.append(usrAge1,usrAge2,usrAge3,usrBtn,usrResult);
+
+var details1=document.getElementById("box1");
+var details2=document.getElementById("box2");
+
+var details3=document.getElementById("box3");
+var output=document.getElementById("content");
+
+usrBtn.addEventListener("click",function(){
+	
+	
+	var person1=details1.value;
+	
+	var person2=details2.value;
+	var person3=details3.value;
+	details1.value="";
+	details2.value="";
+	details3.value="";
+	
+
+	
+	if(person1<person2 && person1<person3){
+		if(person2<person3){
+			output.value="person1:"+person1+","+"person2:"+person2+","+"person3:"+person3;
+		}
+		else{
+			output.value=person1,person3+",person2:"+person2;
+		}
+	}
+	else if(person2<person1 && person2<person3){
+		if(person1<person3){
+			output.value="person2:"+person2+","+"person1:"+person1+","+"person3:"+person3;
+		}
+		else{
+			output.value="person2:"+person2+","+"person3:"+person3+","+"person1:"+person1;
+		}
+	}
+	else if(person3<person1 && person3<person2){
+		if(person1<person2){
+			output.value="person3:"+person3+","+"person1:"+person1+","+"person2:"+person2;
+		}
+		else{
+			output.value="person3:"+person3+","+"person2:"+person2+","+"person1:"+person1;
+		}
+	}
+	
+});
 
 
 
@@ -51,51 +104,3 @@ usrResult.style.paddingTop="11px";
 usrBtn.style.padding="5px";
 usrBtn.style.borderRadius="16px";
 usrBtn.style.backgroundImage="linear-gradient(to right,#c8e13e82, #ff0099)";
-
-
-
-
-
-usrInp.append(usrAge1,usrAge2,usrAge3,usrBtn,usrResult);
-
-var details1=document.getElementById("box1");
-var details2=document.getElementById("box2");
-var details3=document.getElementById("box3");
-var output=document.getElementById("view");
-
-usrBtn.addEventListener("click",function(){
-	var person1=details1.value;
-	var person2=details2.value;
-	var person3=details3.value;
-	details1.value="";
-	details2.value="";
-	details3.value="";
-	
-
-	
-	if(person1<person2 && person1<person3){
-		if(person2<person3){
-			output.value="person1:"+person1+","+"person2:"+person2+","+"person3:"+person3;
-		}
-		else{
-			output.value="person1:"+person1+","+"person3:"+person3+","+"person2:"+person2;
-		}
-	}
-	else if(person2<person1 && person2<person3){
-		if(person1<person3){
-			output.value="person2:"+person2+","+"person1:"+person1+","+"person3:"+person3;
-		}
-		else{
-			output.value="person2:"+person2+","+"person3:"+person3+","+"person1:"+person1;
-		}
-	}
-	else if(person3<person1 && person3<person2){
-		if(person1<person2){
-			output.value="person3:"+person3+","+"person1:"+person1+","+"person2:"+person2;
-		}
-		else{
-			output.value="person3:"+person3+","+"person2:"+person2+","+"person1:"+person1;
-		}
-	}
-	
-});
